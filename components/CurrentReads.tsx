e2d81f89a13c2f9b96fa7e589024fa68260a4670
Vansh <vanshmorwani1@gmail.com>
2025-09-22 23:12:34 +0530

    added books section

---
+++ components/CurrentReads.tsx
@@ -0,0 +1,26 @@
+import { books } from "@/utils/data";
+import Image from "next/image";
+
+export default function CurrentReads() {
+  return (
+    <div className="w-full my-4">
+      <h1 className="text-white text-xl font-bold mb-2">What I’m Reading</h1>
+      <div className="w-full h-fit overflow-hidden">
+        <div
+          className={`flex items-center gap-1 book-slide h-full hover:pause-scroll`}
+        >
+          {books.concat(books).map((link, idx) => (
+            <Image
+              className="grayscale-100 hover:grayscale-0 h-42 w-full"
+              alt={`book ${idx + 1}`}
+              key={idx}
+              height={176}
+              width={144}
+              src={link}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
