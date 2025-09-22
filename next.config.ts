8951f7fc7a8a95cb14f570517b7a1f126e103170
Vansh <vanshmorwani1@gmail.com>
2025-09-22 23:11:39 +0530

    config changes

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images:{
+    remotePatterns:[
+      {
+        hostname:"res.cloudinary.com"
+      }
+    ]
+  }
   /* config options here */
 };
 
