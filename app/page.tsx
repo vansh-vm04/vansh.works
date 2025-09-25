4b6b5cdd64212be00053e22cf6c286f5176892c4
Vansh <vanshmorwani1@gmail.com>
2025-09-25 23:27:01 +0530

    page update

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
+import Contact from "@/components/Contact";
+import CurrentReads from "@/components/CurrentReads";
 import Introduction from "@/components/Introduction";
+import Projects from "@/components/Projects";
+import RandomQuotes from "@/components/RandomQuotes";
 import Skills from "@/components/Skills";
 
 export default function Home() {
   return <div className="w-full h-full flex flex-col items-center">
     <Introduction/>
     <Skills/>
+    <Projects/>
+    <CurrentReads/>
+    <RandomQuotes/>
+    <Contact/>
   </div>;
 }
