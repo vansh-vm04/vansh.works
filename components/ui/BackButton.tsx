fa89e32215679a43f77da0f1dafeaba41c31be6a
Vansh <vanshmorwani1@gmail.com>
2025-09-26 23:25:47 +0530

    back btn

---
+++ components/ui/BackButton.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+export function BackButton({ link }: { link: string }) {
+  return (
+    <Link className="flex items-center gap-1 text-zinc-400 text-md" href={link}>
+      <svg
+        className="stroke-zinc-400 size-4 stroke-2"
+        fill="none"
+        height="24"
+        viewBox="0 0 24 24"
+        width="24"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <polyline points="9 14 4 9 9 4" />
+        <path d="M20 20v-7a4 4 0 0 0-4-4H4" />
+      </svg>{" "}
+      back
+    </Link>
+  );
+}
