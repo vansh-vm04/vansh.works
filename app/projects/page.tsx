fc0b0e2fe7262c538dcf49c6033dea5146ce46d1
Vansh <vanshmorwani1@gmail.com>
2026-05-17 03:09:07 +0530

    feat: adjust Projects component layout; remove responsive grid for a simpler design

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -9,7 +9,7 @@ export default function Projects() {
         <BackButton link="/#projects" />
       </div>
       <h1 className="text-white text-xl font-bold mb-4">All Projects</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+      <div className="grid grid-cols-1 gap-4">
         {projects.map((project, idx) => (
           <ProjectCard key={idx} project={project} />
         ))}
